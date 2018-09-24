023418ff349d395284a541b2a77eb56d294f58c8
sxuhms <heath_shults@fanniemae.com>
2018-09-24 01:16:08 -0500

    updated js files

---
+++ src/assets/scripts/vendor/02-framekit/forms.js
@@ -0,0 +1,40 @@
+function handleFormSubmission(){
+  $('.fk-form input').on('keyup', function(e){
+    $(this).addClass('edited')
+  })
+
+  $('.fk-form [type="submit"]').on('click.formValidation', function(e){
+    shouldPrevent = false
+    errorList = []
+
+
+    $form = $(this).parents('form')
+    $inputs = $form.find('input[required], textarea[required], select[required]')
+
+    $inputs.each(function(index, input){
+      errorMessageSelector = 'label[for="' + $(input).attr('id') + '"] .fk-error-message'
+      $form.find(errorMessageSelector).removeAttr('aria-live')
+      error = $form.find(errorMessageSelector)
+      error.css('display', 'none')
+
+      if(!input.validity.valid){
+        error.css('display', 'inline-block')
+        shouldPrevent = true
+        errorList.push(error)
+
+      }
+      else {
+        error.css('display', 'none')
+      }
+    })
+
+    if(!$form[0].checkValidity()){
+      e.preventDefault()
+      errorList[0].attr('aria-live', 'assertive')
+    }
+  })
+}
+
+$(document).ready(function(){
+  handleFormSubmission()
+})
