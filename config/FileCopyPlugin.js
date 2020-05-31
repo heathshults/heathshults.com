8e85d7f35370820ed972812807aa89385f40fe87
root <root@musicman1.localdomain>
2020-05-31 06:57:15 -0500

    cards

--- config/FileCopyPlugin.js
+++ config/FileCopyPlugin.js
@@ -1,21 +1,24 @@
 const path = require('path')
+const fs = require('fs-extra')
 const { exec } = require('child_process');
 const validateOptions = require('schema-utils');
+const glob = require('glob');
 
+var files = []
+var options = []
+var rootDir = path.resolve(process.cwd())
 let tasks = []
 
 const pluginName = 'FileCopyPlugin';
 
 class FileCopyPlugin {
   constructor(options = {}){
-    tasks = options || [];
+    files = options || [];
   }
   apply(compiler) {
     compiler.hooks.run.tap(pluginName, compilation => {
       
-
-      
-      const Copier = (options = []) => {
+        const Copier = (options = []) => {
             tasks = options
             tasks.map(task => {
             console.log(`copying ${task}...`)
@@ -25,6 +28,9 @@ class FileCopyPlugin {
           })
       }
       Copier(tasks)
+      
+      
+     
 
     });
   }
@@ -32,7 +38,56 @@ class FileCopyPlugin {
 
 module.exports = FileCopyPlugin;
 
+//==========================================
+// const path = require('path')
+// const fs = require('fs-extra')
+// const { exec } = require('child_process');
+// const validateOptions = require('schema-utils');
+// const glob = require('glob');
 
+// var files = []
+// var options = []
+// var rootDir = path.resolve(process.cwd())
+
+// var filesToCopy = [
+//   {
+//       from: `${rootDir}/src/css/**/*.{css,map}`,
+//       to: `${rootDir}/www/css`,
+//       ignore: ['heathshults.css', 'heathshults.min.css'] 
+//     },
+//     {
+//       from: `${rootDir}/content/**/*`,
+//       to: `${rootDir}/www/content`,
+//       ignore: ['**/*.zip'] 
+//     },
+//     {
+//       from: `${rootDir}/img/**/*.{png,jpg,gif,svg}`,
+//       to: `${rootDir}/img`,
+//       ignore: ['**/*.ejs'] 
+//     }
+//   ]
+// console.log(filesToCopy)
+//   const filterFunc = (src, dest) => {
+//     console.log(src)
+//     console.log(dest)
+//   }
+//     const Copier = (options = []) => {
+//       var opts = options
+//       opts.forEach((item, i) => {
+//         var src = item[i]
+//         console.log(src)
+//         // var src = item[i].from
+//         //var dest = item[i].to
+//         //var ignore = item[i].ignore
+
+//         // fs.copy(src, dest, { filter: filterFunc }, err => {
+//         fs.copy(`${rootDir}/src`, `${rootDir}/.backup`, { filter: filterFunc }, err => {
+//           if (err) return console.error(err)
+//           console.log('success!')
+//         })
+//       })
+//     }
+// Copier(filesToCopy)
 
 
 
---
+++ winpack.customServer.js
@@ -0,0 +1,285 @@
+const path = require('path');
+const glob = require('glob');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const { exec } = require('child_process')
+const gulpfile = require('./gulpfile.babel.js')
+const FileCopyPlugin = require('./config/FileCopyPlugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+// const OfflinePlugin = require('offline-plugin/runtime').install();
+
+const isDev = (process.env.NODE_ENV === 'development');
+const basePath = process.cwd();
+
+const options = { 
+  hmr: true,
+  liveReload: true,
+  open: true
+ };
+
+//#region 
+//  const del = require('del');
+ 
+// (async cleanup() {
+//     const deletedPaths = await del(['temp/*.js', '!temp/unicorn.js']);
+ 
+//     console.log('Deleted files and directories:\n', deletedPaths.join('\n'));
+// })();
+// const nunjucksContext = require('./resources/data/index');
+// const nunjucksDevConfig = require('./resources/html/config.dev.json');
+// const nunjucksProdConfig = require('./resources/html/config.prod.json');
+
+// nunjucksContext.config = (isDev) ? nunjucksDevConfig : nunjucksProdConfig;
+
+// const nunjucksOptions = JSON.stringify({
+//   searchPaths: `${basePath}/resources/html/`,
+//   context: nunjucksContext,
+// });
+
+// const pages = glob.sync('**/*.njk', {
+//   cwd: path.join(basePath, 'resources/html/pages/'),
+//   root: '/',
+// }).map(page => new HtmlWebpackPlugin({
+//   filename: page.replace('njk', 'html'),
+//   template: `resources/html/pages/${page}`,
+// }));
+//#endregion
+
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: [
+      path.join(__dirname, 'src', 'index.js'),
+    ]
+  },
+  // watch: true,
+  output: {
+    path: path.resolve(process.cwd(), 'www'),// path.resolve(process.cwd(), 'dist')
+    // publicPath: '/assets/',
+    filename: "HeathScript.built.js",
+    chunkFilename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.(jsx|js)?$/,
+        include: [ path.resolve(__dirname, 'src/lib') ],
+        exclude: [ path.resolve(__dirname, 'node_modules') ],
+        loader: 'babel-loader',
+        options: {
+          presets: [ "@babel/preset-env" ]
+        }
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      // {
+      //   test: /\.(njk|nunjucks)$/,
+      //   loader: ['html-loader', `nunjucks-html-loader?${nunjucksOptions}`],
+      // },
+      
+      {
+        test: /\.(png|jpe?g|gif|eot|ttf|woff|woff2|otf|json)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: false,
+          },
+        }, ],
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(
+      // {
+      //   cleanOnceBeforeBuildPatterns: ['**/*', '!content'],
+      //   verbose: true,
+      //   protectWebpackAssets: false
+      // }
+    ),
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: path.resolve(__dirname, 'www/index.html'),
+      template: 'src/index.ejs',
+      favicon: 'src/favicon.ico'
+      // minify: { removeComments: true, collapseWhitespace: true, removeAttributeQuotes: true }
+    }),
+    // new HtmlWebpackPlugin({
+    //   filename: path.resolve(__dirname, './www/index-allcode.html'),
+    //   template: 'src/index-allcode.ejs',
+    // }),
+    // copy assets and manifest.json
+    
+  //   new CopyPlugin([
+  //    {
+  //     from: `${path.resolve(process.cwd(), 'src/css')}/**/*.{css,map}`,
+  //     to: path.resolve(process.cwd(), 'www/css'),
+  //     ignore: ['heathshults.css', 'heathshults.min.css'] 
+  //   },
+  //   {
+  //     from: 'content/**/*',
+  //     // to: '',
+  //     ignore: ['**/*.zip'] 
+  //   },
+  //   {
+  //     from: 'img/**/*.{png,jpg,gif,svg}',
+  //     // to: '',
+  //     ignore: ['**/*.ejs'] 
+  //   },
+  //   {
+  //     from: 'js/**/*',
+  //     // to: '.',
+  //     ignore: ['HeathScript.js', 'contact_me.js', 'jqBootstrapValidation.js']
+  //   },
+  //   {
+  //     from: 'DevBox/**/*',
+  //     // to: 'DevBox/',
+  //     ignore: ['**/*.ejs']
+  //   },
+  //   {
+  //     from: 'lib/**/*.{js,json}',
+  //     // to: 'lib/',
+  //     ignore: ['**/*.ejs']
+  //   },
+  //   {
+  //     from: 'vendor/**/*',
+  //     // to: 'vendor/',
+  //     ignore: ['**/*.ejs']
+  //   }
+  // ]),
+    new FileCopyPlugin([
+      {mode: 'default'},
+      {
+        from: `${path.resolve(process.cwd(), 'src/css')}/**/*.{css,map}`,
+        to: path.resolve(process.cwd(), 'www/css'),
+        ignore: ['heathshults.css', 'heathshults.min.css'] 
+      },
+    ]),
+    // new FileCopyPlugin(
+    //   [
+    //     'copy_img',
+    //     'copy_js',
+    //     'copy_vendor',
+    //     'copy_css',
+    //     'copy_components'
+    //   ]
+    // ),
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        host: 'localhost',
+        port: 9900,
+        proxy: 'http://localhost:9901/',
+        // injectCss: true,
+        // files: [
+        //   {
+        //     match: ['src/*.html', 'src/scss/*.scss', 'src/css/*.css', 'src/js/*.js', 'src/js/*.json'],
+        //     fn: function (event, file) {
+        //       if (event === 'change') {
+        //         exec('./node_modules/.bin/webpack --config webpack.customServer.js --mode development --display-error-details --colors', (error, stdout, stderr) => {
+        //           if (error) {
+        //               console.log(`error: ${error.message}`);
+        //               return;
+        //           }
+        //           if (stderr) {
+        //               console.log(`stderr: ${stderr}`);
+        //               return;
+        //           }
+        //           console.log(`stdout: ${stdout}`);
+        //         });
+        //         console.log('Processed '+file)
+        //       }
+        //     }
+        //   }
+        // ],
+        reloadDelay: 2000
+    }),
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true)
+    }),
+    // Ignore node_modules so CPU usage with poll
+    // watching drops significantly.
+    new webpack.WatchIgnorePlugin([
+      path.join(__dirname, "node_modules")
+    ]),
+  ],
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    stats: "errors-only",
+    contentBase: path.join(__dirname, 'www/'),
+    inline: false,
+    host: 'localhost',
+    port: 9901,
+    compress: true,
+    liveReload: true,
+    writeToDisk: true
+  }
+}
+
+
+// if (!isDev) {
+//   module.exports.plugins.push(
+//     new CleanWebpackPlugin(['dist']),
+//     new webpack.optimize.UglifyJsPlugin(),
+//   );
+// }
+/*
+function copy_images() {
+  console.log('copying images...')
+  exec('./node_modules/.bin/gulp copy_img', 
+  (error) => { if (error) {console.log(`error: ${error.message}`)}
+  });
+  callbackify()
+}
+
+function copy_js() {
+  console.log('copying javascript files...')
+  exec('./node_modules/.bin/gulp copy_js', 
+    (error) => { if (error) {console.log(`error: ${error.message}`)}
+  });
+  callbackify()
+}
+
+function copy_vendor() {
+  console.log('copying vendor files...')
+  exec('./node_modules/.bin/gulp copy_vendor', 
+    (error) => { if (error) {console.log(`error: ${error.message}`)}
+  });
+  callbackify()
+}
+
+function copy_css() {
+  console.log('copying css...')
+  exec('./node_modules/.bin/gulp copy_css', 
+    (error) => { if (error) {console.log(`error: ${error.message}`)}
+  });
+  callbackify()
+}
+let copy_tasks = [
+  'copy_img',
+  'copy_js',
+  'copy_vendor',
+  'copy_css'
+]
+
+
+
+const Copier = (options = []) => {
+      let tasks = options
+      tasks.map(task => {
+      console.log(`copying ${task}...`)
+      exec(`./node_modules/.bin/gulp ${task}`, (error) => { 
+          if (error) {console.log(`error: ${error.message}`)}
+      })
+    })
+}
+Copier(copy_tasks)
+*/
